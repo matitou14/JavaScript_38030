811d3dc3f1823d59c2e29c72e8280b0ba28f464c
Matias Rodriguez <mat.rodri@gmail.com>
2022-08-17 01:55:26 -0300

    index.html:maquetado de la app

--- js/variables.js
+++ js/variables.js
@@ -11,6 +11,8 @@
 
 //Variables para la ventana Login
 
+// let input1 = document.getElementById("nombre");
+// input1.addEventListener("input", () => {console.log(input1.value)});
 
 
 //  let btn = document.querySelector("#new-quote");
@@ -36,3 +38,36 @@
 //     person.innertext = quotes[random].person;
 //  });
 
+(function ($) {
+    $.fn.arctic_scroll = function (options) {
+
+        var defaults = {
+            elem: $(this),
+            speed: 500
+        };
+        var options = $.extend(defaults, options);
+
+        options.elem.click(function(event){     
+            event.preventDefault();
+            var offset = ($(this).attr('data-offset')) ? $(this).attr('data-offset') : false,
+                position = ($(this).attr('data-position')) ? $(this).attr('data-position') : false;         
+            if (offset) {
+                var toMove = parseInt(offset);
+                $('html,body').stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, options.speed);
+            } else if (position) {
+                var toMove = parseInt(position);
+                $('html,body').stop(true, false).animate({scrollTop: toMove }, options.speed);
+            } else {
+                $('html,body').stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, options.speed);
+            }
+        });
+
+    };
+})(jQuery);
+
+$(function(){
+    $(".scroller").arctic_scroll({
+        speed: 600
+
+    });
+});
